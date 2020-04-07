50c2d20a3add66d0cabc4e9cf0513cfce67f8cfb
Craig Hutchinson <54269136+CraigHutchinson@users.noreply.github.com>
2020-04-07 10:27:36 +0100

    Fixed negation to -1

--- include/sub0pub/sub0pub.hpp
+++ include/sub0pub/sub0pub.hpp
@@ -439,7 +439,7 @@ namespace sub0
             Subscribe<Data>** const iBegin = state_.subscriptions;
             Subscribe<Data>** const iEnd = iBegin + state_.subscriptionCount;
             Subscribe<Data>** const iPend = std::remove(iBegin, iEnd, subscriber );
-            assert(std::distance(iEnd - iPend) == 1);
+            assert(std::distance(iEnd, iPend) == -1);
             --state_.subscriptionCount;
         }
 
